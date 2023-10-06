acc0efb3a98160454ffc88de13b19c95d93e7d3a
Kallan Duong <kallan@student-10-201-24-145.wireless.adelaide.edu.au>
2023-10-06 10:47:57 +1030

    added particle classs

--- pso_variants.py
+++ pso_variants.py
@@ -4,6 +4,16 @@ import random
 #assumes that particle class has the following variables: position, velocity, best individual position, best position of any neighbours
 #assumes fitness has been implemented elsewhere and can be imported
 
+class Particle:
+    def __init__(self,num_dimensions):
+        self.velocity = []
+        self.position = []
+        self.num_dimensions = num_dimensions
+        self.velocity = random.uniform(-100,100,num_dimensions)
+        self.position = random.uniform(-100,100,num_dimensions)
+        self.best_position = self.position
+        self.best_neighbour_position = self.position
+
 
 class PSO:
     def __init__(self, problem, num_particles, max_iterations, topology):
@@ -39,8 +49,8 @@ class PSO:
     def update(self, particle_index):
         particle = self.problem[particle_index]
         #Vid = Vid + Ce1(Pid-Xid) + Ce2(Pgd-Xid)
-        e1 = random.uniform(0,1,particle.num_dimenstions)
-        e2 = random.uniform(0,1,particle.num_dimenstions)
+        e1 = random.uniform(0,1,particle.num_dimensions)
+        e2 = random.uniform(0,1,particle.num_dimensions)
         for d in particle.num_dimensions:
             particle.velocity[d] = particle.velocity[d] + 2*e1[d]*(particle.best_position[d]-particle.position[d]) + 2*e2[d]*(particle.best_neighbour_position[d]-particle.position[d])
             particle.position[d] = particle.position[d] + particle.velocity[d]
