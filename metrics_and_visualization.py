9861bebe49953691f5a711ba85d6be9dd1aec708
Kiratss <127015455+Kiratss@users.noreply.github.com>
2023-10-06 10:38:31 +1030

    Graph 1 Done

--- metrics_and_visualization.py
+++ metrics_and_visualization.py
@@ -1,7 +1,23 @@
 import matplotlib.pyplot as plt
+import distinctpy
 
 def plot_metrics(metrics_data):
-    pass
+    iterations = metrics_data['iterations']
+    fitness_values = metrics_data['fitness_values']
+    
+    # Create a new figure
+    plt.figure(figsize=(10, 5))
+    
+    # Create the first sub-figure 
+    plt.subplot(1, 1, 1)
+    plt.plot(iterations, fitness_values, marker='o', linestyle='-')
+    plt.title('Current Best Fitness Value Over Iterations')
+    plt.xlabel('Iterations')
+    plt.ylabel('Fitness Value')
+    
+    # Create and display the graph
+    plt.tight_layout()  
+    plt.show()
 
 def gather_metrics(swarm):
     pass
